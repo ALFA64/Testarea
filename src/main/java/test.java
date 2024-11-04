31fe3b7028731be63b8aac487996be2c42a30932
ALFA64 <stefangodoroja64@gmail.com>
2024-11-04 12:03:55 +0200

    primul commit

---
+++ src/main/java/test.java
@@ -0,0 +1,26 @@
+package org.mgorbatin;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class test {
+    public static void main(String[] args){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\stefa\\Downloads\\chromedriver-win64\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://www.google.com");
+        try{
+            Thread.sleep(2000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+
+        driver.close();
+    }
+}
